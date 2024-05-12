d470ce0adb89716dd9e99532b59ed09e5f588514
MaxOnn10 <maks.melenevskiy@gmail.com>
2024-05-13 01:52:34 +0300

    finished burger menu + navigation search

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,34 @@
 const burgerMenu = document.querySelector('.header__burger');
 const burgerContainer = document.querySelector('.header__burger-container');
 const nav = document.querySelector('.header__nav');
+const headerTop = document.querySelector('.header__top');
+const headerButton = document.querySelector('.header__button-container');
+const burgerItem = document.querySelectorAll('.header__list-item');
+
+
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('header__burger-active');
   burgerContainer.classList.toggle('header__burger-container--active');
   nav.classList.toggle('header__nav--active');
+  headerButton.classList.toggle('header__button-container--active');
+  headerTop.classList.toggle('header__top--active');
   // headerTop.classList.toggle('header__top--active');
   // body.classList.toggle('disable-scroll');
 });
 
+burgerItem.forEach(function (item) {
+  item.addEventListener('click', function () {
+    burgerMenu.classList.remove('header__burger-active');
+    burgerContainer.classList.remove('header__burger-container--active');
+    nav.classList.remove('header__nav--active');
+    headerButton.classList.remove('header__button-container--active');
+    headerTop.classList.remove('header__top--active');
+  });
+});
+
 const viewers = document.querySelectorAll(".image-compare");
-  
+
 const options = {
 
   // UI Theme Defaults
@@ -38,7 +55,7 @@ const options = {
   startingPoint: 50,
   fluidMode: false
 };
-  
+
 // Add your options object as the second argument
 viewers.forEach((element) => {
   let view = new ImageCompare(element).mount();
