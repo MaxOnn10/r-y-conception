f24db8e9872e9b0aedf0f1a49a1037431df52e90
MaxOnn10 <maks.melenevskiy@gmail.com>
2024-05-12 20:19:45 +0300

    different fixes + map

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,31 @@
 const viewers = document.querySelectorAll(".image-compare");
   
-const options = {
+// const options = {
 
-  // UI Theme Defaults
-  controlColor: "#fff",
-  controlShadow: false,
-  addCircle: false,
-  addCircleBlur: false,
+//   // UI Theme Defaults
+//   controlColor: "#fff",
+//   controlShadow: false,
+//   addCircle: false,
+//   addCircleBlur: false,
 
-  // Label Defaults
-  showLabels: false,
-  labelOptions: {
-    before: 'Before',
-    after: 'After',
-    onHover: false
-  },
+//   // Label Defaults
+//   showLabels: false,
+//   labelOptions: {
+//     before: 'Before',
+//     after: 'After',
+//     onHover: false
+//   },
 
-  // Smoothing
-  smoothing: true,
-  smoothingAmount: 100,
+//   // Smoothing
+//   smoothing: true,
+//   smoothingAmount: 100,
 
-  // Other options
-  hoverStart: false,
-  verticalMode: false,
-  startingPoint: 50,
-  fluidMode: false
-};
+//   // Other options
+//   hoverStart: false,
+//   verticalMode: false,
+//   startingPoint: 50,
+//   fluidMode: false
+// };
   
 // Add your options object as the second argument
 viewers.forEach((element) => {
