559e9f019f782fce766d3ef151009ea092f1d926
MaxOnn10 <maks.melenevskiy@gmail.com>
2024-05-12 23:45:01 +0300

    add footer + icons

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,43 @@
+const burgerMenu = document.querySelector('.header__burger');
+const burgerContainer = document.querySelector('.header__burger-container');
+const nav = document.querySelector('.header__nav');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('header__burger-active');
+  burgerContainer.classList.toggle('header__burger-container--active');
+  nav.classList.toggle('header__nav--active');
+  // headerTop.classList.toggle('header__top--active');
+  // body.classList.toggle('disable-scroll');
+});
+
 const viewers = document.querySelectorAll(".image-compare");
   
-// const options = {
+const options = {
 
-//   // UI Theme Defaults
-//   controlColor: "#fff",
-//   controlShadow: false,
-//   addCircle: false,
-//   addCircleBlur: false,
+  // UI Theme Defaults
+  controlColor: "#fff",
+  controlShadow: false,
+  addCircle: false,
+  addCircleBlur: false,
 
-//   // Label Defaults
-//   showLabels: false,
-//   labelOptions: {
-//     before: 'Before',
-//     after: 'After',
-//     onHover: false
-//   },
+  // Label Defaults
+  showLabels: false,
+  labelOptions: {
+    before: 'Before',
+    after: 'After',
+    onHover: false
+  },
 
-//   // Smoothing
-//   smoothing: true,
-//   smoothingAmount: 100,
+  // Smoothing
+  smoothing: true,
+  smoothingAmount: 100,
 
-//   // Other options
-//   hoverStart: false,
-//   verticalMode: false,
-//   startingPoint: 50,
-//   fluidMode: false
-// };
+  // Other options
+  hoverStart: false,
+  verticalMode: false,
+  startingPoint: 50,
+  fluidMode: false
+};
   
 // Add your options object as the second argument
 viewers.forEach((element) => {
